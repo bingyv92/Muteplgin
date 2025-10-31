b890ca31f8f2310c92584b09f2e35c5e40524d67
bingyv92 <152452349+bingyv92@users.noreply.github.com>
2025-10-31 20:43:05 +0800

    Fix formatting of plugin metadata name

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from src.plugin_system.base.plugin_metadata import PluginMetadata
 
 __plugin_meta__ = PluginMetadata(
-    name="群聊禁言管理插件 (Mute Plugin)",
+    name="群聊禁言管理插件 Mute Plugin",
     description="群聊禁言管理插件",
     usage="在群聊中提供智能禁言功能",
     version="4.2.0",
     author="SengokuCola",
     license="MIT",
 )
+
